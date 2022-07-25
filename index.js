b7130803cfc1461773f8e1c2e2d5ab65724f978a
Zachary Farrell <zfarrell0507@gmail.com>
2022-07-25 14:09:41 -0400

    commit tree 2f3e8f2b10e11d88b59e724baecbe4437b011010

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(books => renderBooks(books));
 }
 
 function renderBooks(books) {
